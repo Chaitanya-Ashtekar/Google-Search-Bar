079b3d80257cbd3a38cad35587a8fb74eb96987c
Chaitanya Ashtekar <85038600+Chaitanya-Ashtekar@users.noreply.github.com>
2021-08-12 22:03:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var searchBox = document.getElementById("searchBox");
+var googleIcon = document.getElementById("google-icon");
+
+googleIcon.onclick = function (){
+  searchBox.classList.toggle("active");
+}
